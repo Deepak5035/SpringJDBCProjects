622abb8519cd0b24170a35ab1d7e20e5e4179a76
Deepak <54569343+Deepak5035@users.noreply.github.com>
2020-06-18 14:10:10 +0530

    commit 2

--- SpringJDBCProj3-CallBackInterface-RowMapper/src/main/java/com/nt/test/Client.java
+++ SpringJDBCProj3-CallBackInterface-RowMapper/src/main/java/com/nt/test/Client.java
@@ -4,17 +4,23 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.nt.dto.EmployeeDto;
+import com.nt.service.EmployeeService;
+
 public class Client {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx=null;
+		EmployeeService empservice=null;
 		
 		ctx=new ClassPathXmlApplicationContext("com/nt/cfgs/applicationContext.xml");
 		
+		empservice=ctx.getBean("empservice",EmployeeService.class);
 		
 		
+		EmployeeDto dto=empservice.getEmpDetils(7499);
 		
-		
+		System.out.println(dto.toString());
 		
 		((AbstractApplicationContext) ctx).close();
 	}
